42c3c5fb080f1161df3dd31c51b367dfbf92abb9
yichenljing <40904846+yichenljing@users.noreply.github.com>
2019-11-11 15:32:30 -0500

    change object

---
+++ week10/Assets/scripts/TimerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+
+public class TimerScript : MonoBehaviour
+{
+    public GameObject timerObject;
+    Text timerTextComp;
+    int timer = 0;
+    float spawnTime = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerTextComp = timerObject.GetComponent<Text>();
+        timerTextComp.text = "pancake";
+        InvokeRepeating("trigger spawn", spawnTime);
+    }
+    private void Update()
+    {
+        if (Input.anyKey) {
+            CancelInvoke();
+        }
+    }
+    // Update is called once per frame
+    void UpdateText()
+    {
+        timer++;
+        timerTextComp.text = "Time: " + timer;
+    }
+    void TriggerSpawn() {
+        SpawnerScript.Spawn();
+
+        spawnTime -= .3f;
+        Invoke("TriggerSpawn", spawnTime);
+    }
+}
